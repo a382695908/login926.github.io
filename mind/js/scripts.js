80ff73be5947aee609657356c85f53133f56c88f
login926 <login926@foxmail.com>
2017-01-19 20:58:05 +0800

    Site updated: 2017-01-19 20:57:58

--- mind/js/scripts.js
+++ mind/js/scripts.js
@@ -1,2 +1,5 @@
-$(".Type1").typeIt({whatToType:["如果把今天干的事儿","告诉三年前的我"],typeSpeed:100,delayStart:100}),
-$(".Type2").typeIt({whatToType:["他肯定说我是疯子"],typeSpeed:100,delayStart:4500})
+$(".Type1").typeIt({whatToType:["像是一个傻子"],typeSpeed:100,delayStart:100}),
+$(".Type2").typeIt({whatToType:["天天给仙人掌浇水"],typeSpeed:100,delayStart:2000})
+$(".Type3").typeIt({whatToType:["但我愿意等"],typeSpeed:500,delayStart:4000})
+$(".Type4").typeIt({whatToType:["等仙人掌开花的那一刻"],typeSpeed:100,delayStart:9000})
+$(".Type5").typeIt({whatToType:["now.diff(begin,'days')"],typeSpeed:100,delayStart:9000})
